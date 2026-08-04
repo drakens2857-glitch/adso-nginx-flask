02e89208c7dce0de24068c29701c2b6a93faacc8
Nicolas Bello <draken.s2857@gmail.com>
2026-08-04 14:12:00 -0500

    configurar docker compose nginx y mysql

--- sample_app.py
+++ sample_app.py
@@ -1,25 +1,26 @@
 from flask import Flask, request, render_template, redirect
 import mysql.connector
 
-
+# Crear la aplicación Flask
 sample = Flask(__name__)
 
-
+# Función para conectar a la base de datos MySQL
 def conectar():
     return mysql.connector.connect(
-        host="servidor-bd",
+        host="servidor-bd",      # Nombre del servicio MySQL en docker-compose
         user="root",
         password="123456",
         database="adso_db"
     )
 
-
+# Ruta principal
 @sample.route("/")
 def main():
 
     db = conectar()
     cursor = db.cursor(dictionary=True)
 
+    # Consultar los aprendices registrados
     cursor.execute("""
         SELECT * FROM aprendices
         ORDER BY id DESC
@@ -35,7 +36,7 @@ def main():
         aprendices=aprendices
     )
 
-
+# Ruta para registrar un nuevo aprendiz
 @sample.route("/registrar", methods=["POST"])
 def registrar():
 
@@ -43,35 +44,31 @@ def registrar():
     documento = request.form["documento"]
     ficha = request.form["ficha"]
 
-
     db = conectar()
     cursor = db.cursor()
 
-
+    # Insertar datos en la tabla
     cursor.execute("""
         INSERT INTO aprendices
         (nombre_completo, numero_documento, ficha)
         VALUES (%s, %s, %s)
-    """,
-    (
+    """, (
         nombre,
         documento,
         ficha
     ))
-
 
     db.commit()
 
     cursor.close()
     db.close()
 
-
     return redirect("/")
 
-
+# Ejecutar la aplicación
 if __name__ == "__main__":
     sample.run(
         host="0.0.0.0",
-        port=5050,
+        port=5000,
         debug=True
     )
